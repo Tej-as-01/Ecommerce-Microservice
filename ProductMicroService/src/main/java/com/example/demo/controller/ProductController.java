ac840dcf4a4996098456232f850fec39b6b1f2ae
Tej-as-01 <t32345839@gmail.com>
2025-10-03 15:57:41 +0530

    Update ProductController.java

--- ProductMicroService/src/main/java/com/example/demo/controller/ProductController.java
+++ ProductMicroService/src/main/java/com/example/demo/controller/ProductController.java
@@ -17,31 +17,39 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.repo.Product;
 import com.example.demo.service.ProductService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/products")
+@Tag(name="Product Controller", description="Handles CRUD operations for Products")
 public class ProductController {
 	
 	@Autowired
 	private ProductService productService;
 	
+	@Operation(summary="Get all Products",description="Returns the list of all products present")
 	@GetMapping()
 	public List<Product> getProducts()
 	{
 		return productService.getAllProducts(); 
 	}
 	
+	@Operation(summary = "Get product by ID", description = "Returns a single product by its ID")
 	@GetMapping("/{id}")
 	public Product getProductById(@PathVariable("id") Long id)
 	{
 		return productService.getProductById(id);
 	}
 	
+	@Operation(summary = "Create a new product", description = "Creates a new product and returns the saved entity")
 	@PostMapping()
 	public Product createProduct(@RequestBody Product product)
 	{
 		return productService.createProduct(product);
 	}
 	
+	@Operation(summary = "Update the product by ID", description = "Updates the existing product by its ID")
 	@PutMapping("/{id}")
 	public Product updateProduct(@PathVariable("id") Long id, @RequestBody Product product)
 	{
@@ -49,6 +57,7 @@ public class ProductController {
 		
 	}
 	
+	@Operation(summary = "Delete all products", description = "Deletes all products from the database")
 	@DeleteMapping()
 	public ResponseEntity<String> deleteAllProducts()
 	{
@@ -56,13 +65,14 @@ public class ProductController {
 		return ResponseEntity.status(HttpStatus.OK).body("All products are deleted");
 	}
 	
+	@Operation(summary = "Delete product by ID", description = "Deletes a specific product by its ID")
 	@DeleteMapping("/{id}")
 	public ResponseEntity<String> deleteProductById(@PathVariable("id") Long id)
 	{
 		productService.deleteProductById(id);
 		return ResponseEntity.status(HttpStatus.OK).body("Product with ID "+id+" is deleted");
 	}
-	
+	@Operation(summary = "Reserves the product by ID", description = "Reserves the specified quantity of the product by its ID if available")
 	@PutMapping("/{id}/{quantity}")
 	public ResponseEntity<String> reserveProduct(@PathVariable("id") Long productId, @PathVariable("quantity") int quantity) {
 	    boolean success = productService.reserveProduct(productId, quantity);
@@ -73,6 +83,7 @@ public class ProductController {
 	    }
 	}
 	
+	@Operation(summary = "Restores the product by ID", description = "Restores the specified quantity to the product by its ID")
 	@PutMapping("/restore/{id}/{quantity}")
 	public ResponseEntity<String> restoreProduct(@PathVariable("id") Long id,@PathVariable("quantity") int quantity)
 	{
